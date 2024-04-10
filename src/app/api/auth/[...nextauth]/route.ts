d78cea19e5395eb2124ddaf1d365ba076571ae57
Rudra Pratap <rudrap99.dba@gmail.com>
2024-04-10 13:28:07 +0530

    final changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
 import { mergeAnonymousCartToUserCart } from "@/lib/db/cart";
 import { prisma } from "@/lib/db/prisma";
+import { env } from "@/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
-import { env } from "process";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma) as Adapter,
